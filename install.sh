abfb5b0e015f15e30afeaebe28e9daef2ec51859
Nick-W <nick.wilson@sparkfun.com>
2013-01-14 13:10:30 -0700

    add links

--- install.sh
+++ install.sh
@@ -4,21 +4,29 @@ if [ -e "${HOME}/.ssh/authorized_keys" ]; then
   for key in `cat .ssh/authorized_keys | cut -d' ' -f3`; do if grep -qE "$key\$" "${HOME}/.ssh/authorized_keys"; then echo "  Updating existing ssh key $key"; sed -i "/$key\$/d" "${HOME}/.ssh/authorized_keys"; fi; done
   cat .ssh/authorized_keys >> ~/.ssh/authorized_keys
 fi
+
 echo "Installing .bashrc"
 if [ -e "${HOME}/.bashrc" ]; then
   mv ~/.bashrc ~/.bashrc_old.`date +%s`
 fi
+ln -s `pwd`/.bashrc ../
+
 echo "Installing .gitconfig"
 if [ -e "${HOME}/.gitconfig" ]; then
   mv ~/.gitconfig ~/.gitconfig_old.`date +%s`
 fi
+ln -s `pwd`/.gitconfig ~/
+
 echo "Installing .vimrc"
 if [ -e "${HOME}/.vimrc" ]; then
   mv ~/.vimrc ~/.vimrc_old.`date +%s`
 fi
+ln -s `pwd`/.vimrc ~/
+
 echo "Installing .vim_runtime"
 if [ -e "${HOME}/.vim_runtime" ]; then
   mv ~/.vim_runtime ~/.vim_runtime_old.`date +%s`
 fi
+ln -s `pwd`/.vim_runtime ~/
 
 echo "Done!"
